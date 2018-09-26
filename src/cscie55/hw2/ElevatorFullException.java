7e78e0ba761dfc8bb805ecc9924eb8abf9ca390d
Brendan Murphy <brendanj.murphy@libertymutual.com>
2018-09-26 13:41:29 -0400

    updated elevator full exception

--- src/cscie55/hw2/ElevatorFullException.java
+++ src/cscie55/hw2/ElevatorFullException.java
@@ -9,7 +9,11 @@ package cscie55.hw2;
  * Defines an elevatorFullException class that must be thrown when something out of the ordinary happens
  *******************************************************************************************************/
 
-public class ElevatorFullException  {
+public class ElevatorFullException extends Exception  {
 
+    public ElevatorFullException() {
+        //sends a string as a message to the super class
+        super("The elevator is full");
+    }
 
   }
