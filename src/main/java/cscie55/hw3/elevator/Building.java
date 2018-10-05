4521dd948915d4f9cec125946a7d79557c1cf5c9
Brendan Murphy <bjohnmurphy1984@gmail.com>
2018-10-05 10:27:09 -0400

    bulding restored to how it was in hw2

--- src/main/java/cscie55/hw3/elevator/Building.java
+++ src/main/java/cscie55/hw3/elevator/Building.java
@@ -10,5 +10,62 @@ package cscie55.hw3.elevator;
  */
 
 public class Building {
-  
+
+    /**
+     * HW2 Requirement: A static final field storing the number of floors in the building
+     */
+    public static final int FLOORS = 7;
+
+    private Floor[] floors = new Floor[FLOORS + 1];;
+
+    /**
+     * Requirement: Define a field for tracking the Elevator's current floor
+     */
+    private int currentFloor = 1;
+
+    private Elevator elevator;
+
+
+    /**
+     * HW2 Requirement: The Building constructor creates an Elevator, and one floor for each floor number
+     * (Move the variable currentFloor that was in the Elevator class in homework 1 to the Building class.)
+     */
+    public Building() {
+        Elevator elevator = new Elevator(this);
+        this.elevator = elevator;
+        for (int i = 1; i <=FLOORS; i++) {
+            Floor floor = new Floor(this, i);
+            this.floors[i] = floor;
+        }
+    }
+
+    /**
+     * HW2 Requirement: Returns the building's Elevator
+     */
+    public Elevator getElevator() {
+        return this.elevator;
+    }
+
+    /**
+     * HW2 Requirement: Returns the Floor object for the given floor number
+     *@param floorNumber - Int the number of the floor
+     */
+    public Floor getFloor(int floorNumber) {
+        return this.floors[floorNumber];
+    }
+
+    /**
+     * Gets the current floor number
+     */
+    int getCurrentFloor() {
+        return this.currentFloor;
+    }
+
+    /**
+     * Sets the current floor number
+     */
+    void setCurrentFloor(int floorNum) {
+        this.currentFloor = floorNum;
+    }
+
 }
