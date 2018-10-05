8ac294d79506d78cc2ad74e9e98274fc4c81231d
Brendan Murphy <bjohnmurphy1984@gmail.com>
2018-10-05 10:01:28 -0400

    initial commit

--- src/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"cscie55.hw2"},{"l":"cscie55.hw2test"},{"l":"main.java.cscie55.hw2.zoo.animals"}]
--- src/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"cscie55.hw2","l":"Building"},{"p":"cscie55.hw2","l":"Elevator"},{"p":"cscie55.hw2","l":"ElevatorFullException"},{"p":"cscie55.hw2test","l":"ElevatorTest"},{"p":"cscie55.hw2","l":"Floor"}]
