1e63a55218a66ee9630697c59065e78471c9d5e7
Brendan M <bjohnmurphy1984@gmail.com>
2018-10-09 23:11:58 -0400

    abstraction of Animal class

--- src/main/java/cscie55/zoo/animals/Animal.java
+++ src/main/java/cscie55/zoo/animals/Animal.java
@@ -7,5 +7,15 @@ package cscie55.zoo.animals;
  * CSCIE-55 HW 3
  * date: 10/11/2018
  ******************************/
-public class Animal {
+public abstract class Animal {
+    private String name;
+    private int age;
+    private String color;
+
+    public Animal (String name, Integer age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
 }
